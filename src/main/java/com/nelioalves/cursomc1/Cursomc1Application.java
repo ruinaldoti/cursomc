aac82bf540c3fb0cf44befaeb5df8d1db107499d
ruinaldoti <ruinaldo.ti@gmail.com>
2019-09-09 21:20:32 -0300

    Criando operação de Instaciacao

--- src/main/java/com/nelioalves/cursomc1/Cursomc1Application.java
+++ src/main/java/com/nelioalves/cursomc1/Cursomc1Application.java
@@ -1,13 +1,30 @@
 package com.nelioalves.cursomc1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc1Application {
+import com.nelioalves.cursomc1.domain.Categoria;
+import com.nelioalves.cursomc1.repositories.CategoriaReposytory;
 
+@SpringBootApplication
+public class Cursomc1Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaReposytory categoriaReposytory;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaReposytory.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
